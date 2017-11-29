f018fd0d3cd11f17a2e1bcfe8ffd63295a54ebfe
ukotyada <33871471+ukotyada@users.noreply.github.com>
2017-11-30 02:04:20 +0530

    Update mainCtrl.js

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -251,7 +251,7 @@ myApp.controller("chatbotController", function($scope,$http, $sce)
 		//	};
 				
 			
-		$http.post("https://971667ce.ngrok.io/message",data).then(function(response,err){
+		$http.post("https://dealerportalbot.mybluemix.net/message",data).then(function(response,err){
 			if(err)
 				{
 					$scope.chatbotmessages.append("Please try again");
